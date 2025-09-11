5d4732c9a47d597d34ed17593d1cf570c828a3c8
Kheireddine Boukhatem <105202227+kheireddinebou@users.noreply.github.com>
2025-09-11 18:14:23 +0100

    refactor: the style of theme toggle button

--- client/components/layout/theme-toggle.tsx
+++ client/components/layout/theme-toggle.tsx
@@ -20,8 +20,8 @@ const ThemeToggle: FC<ThemeToggleProps> = ({}) => {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="sm">
-          <Sun className="rotate-0 scale-100 transition-all hover:text-slate-900 dark:rotate-90 dark:scale-0 dark:text-slate-400 dark:hover:text-slate-100" />
-          <Moon className="absolute rotate-90 scale-0 transition-all hover:text-slate-900 dark:rotate-0 dark:scale-100 dark:text-slate-400 dark:hover:text-slate-100" />
+          <Sun className="rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0" />
+          <Moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle Theme</span>
         </Button>
       </DropdownMenuTrigger>
